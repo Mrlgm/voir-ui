c50b540f17244d244beabfd5a0c42fea133be531
Mrlgm <877205619@qq.com>
2018-11-02 01:10:13 +0800

    安装chai

--- src/button-group.vue
+++ src/button-group.vue
@@ -23,7 +23,9 @@
         vertical-align: middle;
         > .vi-button{
             border-radius: 0;
-            margin-left: -1px;
+            &:not(:first-child){
+                margin-left: -1px;
+            }
             &:first-child{
                 border-top-left-radius: var(--border-radius);
                 border-bottom-left-radius: var(--border-radius);
