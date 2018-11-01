e3a7cc949ad45914e343ada92600ccd37b6970be
Mrlgm <877205619@qq.com>
2018-11-02 00:34:23 +0800

    完成button-group创建

---
+++ src/button-group.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="vi-button-group">
+        <slot></slot>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "button-group"
+    }
+</script>
+
+<style lang="scss" scoped>
+    .vi-button-group{
+        display: inline-flex;
+        vertical-align: middle;
+        > .vi-button{
+            border-radius: 0;
+            margin-left: -1px;
+            &:first-child{
+                border-top-left-radius: var(--border-radius);
+                border-bottom-left-radius: var(--border-radius);
+            }
+            &:last-child{
+                border-top-right-radius: var(--border-radius);
+                border-bottom-right-radius: var(--border-radius);
+            }
+            &:hover{
+                position: relative;
+                z-index: 1;
+            }
+        }
+    }
+
+</style>
