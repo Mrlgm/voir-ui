47243ae2e5554cdad1ab3e3385227eecebe19692
Mrlgm <877205619@qq.com>
2018-11-05 00:27:16 +0800

    初步完成row和col

--- src/app.js
+++ src/app.js
@@ -3,11 +3,15 @@ import Button from './button'
 import Icon from './icon'
 import ButtonGroup from './button-group'
 import Input from './input'
+import Row from './row'
+import Col from './col'
 
 Vue.component('vi-button', Button)
 Vue.component('vi-icon', Icon)
 Vue.component('vi-button-group', ButtonGroup)
 Vue.component('vi-input', Input)
+Vue.component('vi-row', Row)
+Vue.component('vi-col', Col)
 
 new Vue({
     el: '#app',
---
+++ src/row.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="row">
+        <slot></slot>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "row"
+    }
+</script>
+
+<style lang="scss" scoped>
+    .row {
+        display: flex;
+    }
+</style>
