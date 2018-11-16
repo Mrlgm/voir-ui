05b57dbd56443a643bacee4bd04993f0b9fc9327
Mrlgm <877205619@qq.com>
2018-11-16 17:07:54 +0800

    实现只创建一个 toast 功能

--- src/plugin.js
+++ src/plugin.js
@@ -1,16 +1,22 @@
 import Toast from './toast'
 
+let currentToast
 export default {
     install(Vue, options) {
         Vue.prototype.$toast = function (message, toastOptions) {
-            let Constructor = Vue.extend(Toast)
-            let toast = new Constructor({
-                propsData: toastOptions
-
-            })
-            toast.$slots.default = [message]
-            toast.$mount()
-            document.body.appendChild(toast.$el)
+            if(currentToast){
+                currentToast.close()
+            }
+            currentToast = createToast({Vue, message, propsData: toastOptions})
         }
     }
+}
+
+function createToast({Vue, propsData, message}) {
+    let Constructor = Vue.extend(Toast)
+    let toast = new Constructor({propsData})
+    toast.$slots.default = [message]
+    toast.$mount()
+    document.body.appendChild(toast.$el)
+    return toast
 }
