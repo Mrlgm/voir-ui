abb62f249907977549969aba695ddb2904132b02
Mrlgm <877205619@qq.com>
2018-11-20 16:37:16 +0800

    tabs 支持 disabled

--- src/tabs-item.vue
+++ src/tabs-item.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="tabs-item" @click="xxx" :class="classes">
+    <div class="tabs-item" @click="onClick" :class="classes">
         <slot></slot>
     </div>
 </template>
@@ -26,7 +26,8 @@
         computed: {
             classes() {
                 return {
-                    active: this.active
+                    active: this.active,
+                    disabled: this.disabled
                 }
             }
         },
@@ -36,7 +37,10 @@
             })
         },
         methods: {
-            xxx() {
+            onClick() {
+                if (this.disabled) {
+                    return
+                }
                 this.eventBus.$emit('update:selected', this.name, this)
             }
         }
@@ -44,17 +48,26 @@
 </script>
 
 <style lang="scss" scoped>
+    $blue: #409EFF;
+    $disabled-color: grey;
     .tabs-item {
-        $blue: #409EFF;
         flex-shrink: 0;
         padding: 0 2em;
         height: 100%;
         display: flex;
         align-items: center;
         cursor: pointer;
+        &:hover{
+            color: $blue;
+        }
         &.active {
             color: $blue;
             font-weight: bold;
+        }
+        &.disabled {
+            color: $disabled-color;
+            cursor: not-allowed;
+            opacity: 0.6;
         }
     }
 </style>
