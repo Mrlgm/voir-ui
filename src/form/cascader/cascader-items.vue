bf02c0856da1ac745f2c608c2602cb2e8246c210
Mrlgm <877205619@qq.com>
2018-12-15 23:35:39 +0800

    确定如何渲染内容

---
+++ src/form/cascader/cascader-items.vue
@@ -0,0 +1,48 @@
+<template>
+    <div class="vi-cascader-items">
+        <div class="left">
+            <div class="label" v-for="item in items" @click="leftSelected = item">
+                {{item.name}}
+            </div>
+        </div>
+        <div class="right" v-if="rightItems">
+            <vi-cascader-items :items="rightItems"></vi-cascader-items>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "ViCascaderItems",
+        props: {
+            items: {
+                type: Array
+            }
+        },
+        data() {
+            return {
+                leftSelected: null
+            }
+        },
+        computed:{
+            rightItems(){
+                if(this.leftSelected && this.leftSelected.children){
+                    return this.leftSelected.children
+                }else{
+                    return null
+                }
+            }
+        }
+    }
+
+</script>
+
+<style lang="scss" scoped>
+    .vi-cascader-items {
+        border: 1px solid red;
+        margin: 10px;
+        display: flex;
+        align-items: flex-start;
+        justify-content: flex-start;
+    }
+</style>
--- src/form/cascader/cascader.vue
+++ src/form/cascader/cascader.vue
@@ -1,35 +1,51 @@
 <template>
     <div class="vi-cascader">
-        <div class="trigger">
+        <div class="trigger" @click="popoverVisible=!popoverVisible">
             <slot></slot>
         </div>
-        <div class="popover">
-            <div v-for="item in source">
-                <vi-cascader-item :sourceItem="item"></vi-cascader-item>
-            </div>
+        <div class="popover" v-if="popoverVisible">
+            <vi-cascader-items :items="source"></vi-cascader-items>
         </div>
     </div>
 </template>
 
 <script>
-    import CascaderItem from './cascader-item'
+    import CascaderItems from './cascader-items'
 
     export default {
         name: "ViCascader",
         components: {
-            'vi-cascader-item': CascaderItem
+            'vi-cascader-items': CascaderItems
         },
         props: {
             source: {
                 type: Array
             }
+        },
+        data() {
+            return {
+                popoverVisible: false,
+            }
+        },
+        computed:{
+
         }
     }
 </script>
 
 <style lang="scss" scoped>
-    .vi-cascader {
+    @import "../../assets/var";
 
+    .vi-cascader {
+        .trigger {
+            border: 1px solid red;
+            height: 32px;
+        }
+
+        .popover {
+            border: 1px solid red;
+            height: 200px;
+        }
     }
 
 </style>
