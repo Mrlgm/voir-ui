ee1ed2920fb6e02804d16299fbe8675108565146
Mrlgm <877205619@qq.com>
2019-01-08 10:19:25 +0800

    创建 Switch 组件

--- src/demo.vue
+++ src/demo.vue
@@ -1,76 +1,20 @@
 <template>
     <div class="demo">
-        <div>
-            <vi-cascader popover-height="200px" :source.sync="source" :selected.sync="selected"
-                         :load-data="loadData"></vi-cascader>
-        </div>
-       <vi-popover>
-           <template>
-               <vi-button>点我</vi-button>
-           </template>
-           <template slot="content">
-               弹出内容
-           </template>
-       </vi-popover>
-
+        <vi-switch v-model="value"></vi-switch>
     </div>
 </template>
 
 <script>
-    import Cascader from './form/cascader/cascader'
-    import Button from './basic/button/button'
-    import db from './db'
-    import Popover from './other/popover/popover'
-
-    function ajax(parentId = 0) {
-        return new Promise((success, fail) => {
-            setTimeout(() => {
-                let result = db.filter((item) => item.parent_id === parentId)
-                result.map(node => {
-                    if (db.filter(item => item.parent_id === node.id).length > 0) {
-                        node.isLeaf = false
-                    } else {
-                        node.isLeaf = true
-                    }
-                })
-                success(result)
-            }, 3000)
-        })
-    }
+    import Switch from './form/switch/switch'
 
     export default {
-        name: "demo",
+        name: 'demo',
         components: {
-            'vi-cascader': Cascader,
-            'vi-button': Button,
-            'vi-popover': Popover
+            'vi-switch': Switch
         },
         data() {
             return {
-                selected: [],
-                source: []
-            }
-        },
-        created() {
-            ajax(0).then((result) => {
-                this.source = result
-            })
-        },
-        methods: {
-            loadData(node, callback) {
-                let {name, id, parent_id} = node
-                ajax(id).then(result => {
-                    callback(result)//调用一下
-                })
-            },
-            xxx() {
-                console.log()
-                ajax(this.selected[0].id).then((result) => {
-                    console.log(result)
-                    let lastLevelSelected = this.source.filter(item => item.id === this.selected[0].id)[0]
-                    //lastLevelSelected.children = result
-                    this.$set(lastLevelSelected, 'children', result)
-                })
+                value: true
             }
         }
     }
---
+++ src/form/switch/switch.vue
@@ -0,0 +1,69 @@
+<template>
+    <div class="vi-switch">
+        <input v-bind:value="value" class="vi-switch_input" type="checkbox">
+        <span class="vi-switch_core" @click="isValue"></span>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "ViSwitch",
+        props: {
+            value: {
+                type: Boolean,
+                default: false
+            },
+            activeColor: {
+                type: String,
+                default: ''
+            },
+            inactiveColor: {
+                type: String,
+                default: ''
+            }
+        },
+        methods: {
+            isValue() {
+                console.log(this.value)
+            }
+        }
+
+    }
+</script>
+
+<style lang="scss" scoped>
+    .vi-switch {
+        display: inline-flex;
+        position: relative;
+
+        .vi-switch_input {
+            position: absolute;
+            width: 0;
+            height: 0;
+            opacity: 0;
+            margin: 0;
+        }
+
+        .vi-switch_core {
+            width: 40px;
+            height: 20px;
+            background: #000;
+            border-radius: 10px;
+            cursor: pointer;
+
+            &::after {
+                content: '';
+                display: block;
+                background-color: #fff;
+                width: 16px;
+                height: 16px;
+                position: absolute;
+                transition: all .3s;
+                top: 2px;
+                left: 2px;
+                border-radius: 50%;
+            }
+        }
+    }
+
+</style>
