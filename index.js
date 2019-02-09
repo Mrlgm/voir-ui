912ee4ec8778ece3a70b687ae1fa101f055967ed
Mrlgm <877205619@qq.com>
2019-02-09 23:35:20 +0800

    添加 install 文件

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import Icon from './src/basic/icon/index.js'
 import Button from './src/basic/button/index.js'
+import ButtonGroup from './src/basic/button-group/index'
 import Switch from './src/form/switch/index.js'
 import Input from './src/form/input/index.js'
 import Cascader from './src/form/cascader/index.js'
@@ -7,17 +8,22 @@ import Grid from './src/layout/grid/index.js'
 import Container from './src/layout/container/index.js'
 import Collapse from './src/layout/collapse/index.js'
 import Tabs from './src/navigation/tabs/index.js'
+import Toast from './src/notice/toast/plugin'
+import Popover from './src/other/popover/index'
 
 const components = [
     Icon,
     Button,
+    ButtonGroup,
     Switch,
     Input,
     Cascader,
     Grid,
     Container,
     Collapse,
-    Tabs
+    Tabs,
+    Toast,
+    Popover
 ]
 
 const install = Vue => {
