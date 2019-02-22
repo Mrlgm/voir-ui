d0ab5babbed70ecbb601217be1029242a8aac3e5
Mrlgm <877205619@qq.com>
2019-02-22 23:29:44 +0800

    添加轮播组件

---
+++ src/other/slides/slides.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="vi-slides">
+
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "ViSlides"
+    }
+</script>
+
+<style lang="scss" scoped>
+    .vi-slides {
+
+    }
+</style>
