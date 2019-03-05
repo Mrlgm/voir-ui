b85eb9a3dcefb7e7194c494f227263696b62e2b8
Mrlgm <877205619@qq.com>
2019-03-05 19:40:39 +0800

    添加文档

---
+++ docs/.vuepress/components/slides-demo.vue
@@ -0,0 +1,39 @@
+<template>
+    <div>
+        <vi-slides class="wrapper" :selected.sync="selected">
+            <vi-slides-item name="1">
+                <div class="box">1</div>
+            </vi-slides-item>
+            <vi-slides-item name="2">
+                <div class="box">2</div>
+            </vi-slides-item>
+            <vi-slides-item name="3">
+                <div class="box">3</div>
+            </vi-slides-item>
+        </vi-slides>
+    </div>
+</template>
+
+<script>
+    import ViSlides from '../../../src/other/slides/slides'
+    import ViSlidesItem from '../../../src/other/slides/slides-item'
+    export default {
+        name: "slides-demo",
+        components: {
+            ViSlides,
+            ViSlidesItem
+        },
+        data() {
+            return {
+                selected: '1'
+            }
+        },
+    }
+</script>
+
+<style lang="scss" scoped>
+    .box {
+        height: 400px;
+        background-color: #ddd;
+    }
+</style>
