9cc05b91bf59c56f25391e4636a1bd527849e3d8
Mrlgm <877205619@qq.com>
2019-04-09 20:12:00 +0800

    初步完成validate，测试驱动开发

--- src/validate.js
+++ src/validate.js
@@ -5,6 +5,26 @@ export default function validate(data, rules) {
         if (rule.required) {
             if (!value && value !== 0) {
                 errors[rule.key] = {required: '必填'}
+                return
+            }
+        }
+        if (rule.pattern) {
+            if (rule.pattern === 'email') {
+                rule.pattern = /^.+@.+$/
+            }
+            if (rule.pattern.test(value) === false) {
+                if (!errors[rule.key]) {
+                    errors[rule.key] = {}
+                }
+                errors[rule.key].pattern = '格式不正确'
+            }
+        }
+        if (rule.minLength) {
+            if (value.length < rule.minLength) {
+                if (!errors[rule.key]) {
+                    errors[rule.key] = {}
+                }
+                errors[rule.key].minLength = '太短'
             }
         }
     })
--- tests/unit/validate.spec.js
+++ tests/unit/validate.spec.js
@@ -12,33 +12,79 @@ describe('validate', () => {
         expect(validate).to.exist
     })
 
-    it('test 1', () => {
-        let data = {
-            email: ''
-        }
+    it('required: true 通过', () => {
+        let data = {email: ''}
 
-        let rules = [{
-            key: 'email',
-            required: true
-        }]
+        let rules = [{key: 'email', required: true}]
 
         let errors = validate(data, rules)
 
         expect(errors.email.required).to.eq('必填')
     })
 
-    it('test 1', () => {
-        let data = {
-            email: 0
-        }
+    it('test email is 0', () => {
+        let data = {email: 0}
 
-        let rules = [{
-            key: 'email',
-            required: true
-        }]
+        let rules = [{key: 'email', required: true}]
 
         let errors = validate(data, rules)
 
         expect(errors.email).to.not.exist
+    })
+
+    it('格式不正确', () => {
+        let data = {email: '@foxmail.com'}
+
+        let rules = [{key: 'email', pattern: /^.+@.+$/}]
+
+        let errors = validate(data, rules)
+        expect(errors.email.pattern).to.eq('格式不正确')
+    })
+
+    it('格式正确', () => {
+        let data = {email: '1@foxmail.com'}
+
+        let rules = [{key: 'email', pattern: /^.+@.+$/}]
+
+        let errors = validate(data, rules)
+        expect(errors.email).to.not.exist
+    })
+
+    it('email格式不正确', () => {
+        let data = {email: '@foxmail.com'}
+
+        let rules = [{key: 'email', pattern: 'email'}]
+
+        let errors = validate(data, rules)
+        expect(errors.email.pattern).to.eq('格式不正确')
+    })
+
+    it('email格式正确', () => {
+        let data = {email: '1@foxmail.com'}
+
+        let rules = [{key: 'email', pattern: 'email'}]
+
+        let errors = validate(data, rules)
+        expect(errors.email).to.not.exist
+    })
+
+    it('pattern & required', () => {
+        let data = {email: ''}
+
+        let rules = [{key: 'email', pattern: 'email', required: true}]
+
+        let errors = validate(data, rules)
+        expect(errors.email.required).to.eq('必填')
+        expect(errors.email.pattern).to.not.exist
+    })
+
+    it('pattern & minLength', () => {
+        let data = {email: ''}
+
+        let rules = [{key: 'email', pattern: 'email', minLength: 6}]
+
+        let errors = validate(data, rules)
+        expect(errors.email.minLength).to.exist
+        expect(errors.email.pattern).to.exist
     })
 })
