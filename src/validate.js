8346ac9934dcb74691494f174cde29a8e6dbb465
Mrlgm <877205619@qq.com>
2019-04-10 19:12:41 +0800

    代码重构

--- src/validate.js
+++ src/validate.js
@@ -3,31 +3,68 @@ export default function validate(data, rules) {
     rules.forEach((rule) => {
         let value = data[rule.key]
         if (rule.required) {
-            if (!value && value !== 0) {
-                errors[rule.key] = {required: '必填'}
+            let error = validate.required(value)
+            if (error) {
+                if (!errors[rule.key]) {
+                    errors[rule.key] = {}
+                }
+                errors[rule.key].required = error
                 return
             }
         }
+        // for(let key in rule){
+        //     if(key !== 'key' && key !== 'required'){
+        //         if (rule[key]) {
+        //             let error = validate[key](value, rule[key])
+        //             if (error) {
+        //                 if (!errors[rule.key]) {
+        //                     errors[rule.key] = {}
+        //                 }
+        //                 errors[rule.key].key = error
+        //             }
+        //         }
+        //     }
+        // }
         if (rule.pattern) {
-            if (rule.pattern === 'email') {
-                rule.pattern = /^.+@.+$/
-            }
-            if (rule.pattern.test(value) === false) {
+            let error = validate.pattern(value, rule.pattern)
+            if (error) {
                 if (!errors[rule.key]) {
                     errors[rule.key] = {}
                 }
-                errors[rule.key].pattern = '格式不正确'
+                errors[rule.key].pattern = error
             }
         }
         if (rule.minLength) {
-            if (value.length < rule.minLength) {
+            let error = validate.minLength(value, rule.minLength)
+            if (error) {
                 if (!errors[rule.key]) {
                     errors[rule.key] = {}
                 }
-                errors[rule.key].minLength = '太短'
+                errors[rule.key].minLength = error
             }
         }
     })
 
     return errors
+}
+
+validate.required = (value) => {
+    if (!value && value !== 0) {
+        return '必填'
+    }
+}
+
+validate.pattern = (value, pattern) => {
+    if (pattern === 'email') {
+        pattern = /^.+@.+$/
+    }
+    if (pattern.test(value) === false) {
+        return '格式不正确'
+    }
+}
+
+validate.minLength = (value, minLength) => {
+    if (value.length < minLength) {
+        return '太短'
+    }
 }
